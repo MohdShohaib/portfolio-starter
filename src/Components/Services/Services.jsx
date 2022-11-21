d2496564c2420164cea9fa99290768ddbe7a227f
MohdShohaib <115138638+MohdShohaib@users.noreply.github.com>
2022-11-21 20:27:07 +0530

    portfoliomy
    portfoliomy

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -6,7 +6,7 @@ import Glasses from "../../img/glasses.png";
 import Humble from "../../img/humble.png";
 import { themeContext } from "../../Context";
 import { motion } from "framer-motion";
-import Resume from './Thankyou..pdf';
+import Resume from './myResume.pdf';
 
 const Services = () => {
   // context
